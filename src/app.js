e7a9bcc1720ef09d4f73674fb11b3f83c9c8ce1e
Luis Sarmiento <48292291+dudunesto@users.noreply.github.com>
2026-01-28 20:02:04 +0000

    Optimización de proyecto

--- src/app.js
+++ src/app.js
@@ -8,17 +8,29 @@ import "./assets/img/4geeks.ico";
 window.onload = function () {
   //write your code here
 
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  let randWho = Math.floor(Math.random() * who.length)
-  let randAction = Math.floor(Math.random() * action.length)
-  let randWhat = Math.floor(Math.random() * what.length)
-  let randWhen = Math.floor(Math.random() * when.length)
-  let newExcuse = who[randWho] + " " + action[randAction] + " " + what[randWhat] + " " + when[randWhen];
+  const newExcuse = randGenerator()
 
+  // Toma el array y selecciona, de manera random, y devuelve uno solo
+  
+   function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)]
+  }
+  
+  // Toma el array random generado en randomArray() y lo devuelve en una sola línea de código
+  function randGenerator() {
+    const randWho = getRandomElement(who)
+    const randAction = getRandomElement(action)
+    const randWhat = getRandomElement(what)
+    const randWhen = getRandomElement(when)
+    return randWho + " " + randAction + " " + randWhat + " " + randWhen;
+  }
+
+// Vincula con html a través de un "textContent"
   document.getElementById("excuse").textContent = newExcuse;
 
 };
